f058e694b5c5273af94d8ddb95d2193c3e886f5a
Bruno Paixao <brunopaixao87@gmail.com>
2018-11-11 21:20:01 -0300

    Arquivos do projeto

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,25 @@
+class SubjectsController < ApplicationController
+  def index
+    @subjects = Subject.sorted
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+end
