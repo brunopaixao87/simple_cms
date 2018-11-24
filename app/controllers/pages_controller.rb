c97c0639d2107dd08e82683ef1ea38d2103c104e
Bruno Paixao <brunopaixao87@gmail.com>
2018-11-24 14:27:00 -0300

    finalizacao do curso

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
 
   layout 'admin'
+
+  #controller filters
+  before_action :confirm_logged_in
+  before_action :find_subject
+  #before_action :find_subjects, :only => [:new, :create, :edit, :update]
+  before_action :set_page_count, :only => [:new, :create, :edit, :update]
   
   def index
-    @pages = Page.sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -11,37 +17,30 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new
-    @page_count = Page.count + 1
-    @subjects = Subject.sorted
+    @page = Page.new(:subject_id => @subject.id )
   end
 
   def create
     @page = Page.new(page_params)
+    @page.subject = @subject
     if @page.save
       flash[:notice] = "Page created successflluy."
-      redirect_to(pages_path)
+      redirect_to(pages_path(:subject_id => @subject.id))
     else
-       @page_count = Page.count + 1
-       @subjects = Subject.sorted
        render('new')
     end
   end
 
   def edit
     @page = Page.find(params[:id])
-    @page_count = Page.count
-    @subjects = Subject.sorted
   end
 
   def update
     @page = Page.find(params[:id])
     if @page.update_attributes(page_params)
       flash[:notice] = "Page updated successflluy."
-      redirect_to(page_path(@page))
+      redirect_to(page_path(@page, :subject_id => @subject.id))
     else
-      @page_count = Page.count
-      @subjects = Subject.sorted
       render('edit')
     end
   end
@@ -54,13 +53,28 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     @page.destroy
     flash[:notice] = "Page destroyed successflluy."
-    redirect_to(pages_path)
+    redirect_to(pages_path(:subject_id => @subject.id))
   end
 
   private 
 
   def page_params
-    params.require(:page).permit(:subject_id, :name, :position, :visible, :permalink)
+    params.require(:page).permit(:name, :position, :visible, :permalink)
+  end
+
+  def find_subject
+    @subject = Subject.find(params[:subject_id]);
+  end
+
+  #def find_subjects
+  #  @subjects = Subject.sorted
+  #end
+
+  def set_page_count
+    @page_count = @subject.pages.count
+    if params[:action] == 'new' || params[:action] == 'create' 
+      @page_count += 1
+    end
   end
 
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,6 +2,8 @@ class AdminUser < ApplicationRecord
 
   # self.table_name = "admin_users"
 
+  has_secure_password 
+
   has_and_belongs_to_many :pages
 
   has_many :section_edits
@@ -24,14 +26,26 @@ class AdminUser < ApplicationRecord
   #validates_confirmation_of :email
 
   # "clean form" validations
-  validates :first_name, :presence => true, :length => { :maximum => 25}
-  validates :last_name, :presence => true, :length => { :maximum => 50}
-  validates :username, :length => { :with => 8..25}, :uniqueness => true
-  validates :email, :presence => true, :length => { :maximum => 100}, :format => EMAIL_REGEX, :confirmation => true
+  validates :first_name, :presence => true, :length => { :maximum => 25 }
+  validates :last_name, :presence => true, :length => { :maximum => 50 }
+  validates :username, :length => { :within => 8..25 }, :uniqueness => true
+  validates :email, :presence => true,
+                    :length => { :maximum => 100 },
+                    :format => EMAIL_REGEX,
+                    :confirmation => true
+
 
   #custom validate
   validate :username_is_allowed
   validate :no_new_users_on_monday, :on => :create
+
+  scope :sorted, lambda { order('last_name ASC, first_name ASC')}
+
+  def name
+    "#{first_name} #{last_name}"
+    # Or: first_name + ' ' + last_name
+    # Or: [first_name, last_name].join(' ')
+  end
 
   private 
 
